af1f1b3f0381d42a2358591e68471fc6c0d64f88
Shubham Jain <shubham.jain@metacube.com>
2019-06-11 11:20:38 +0530

    Add files via upload
    Intial upload

---
+++ WindowsServiceQ1-19/WindowsServiceQ1-19/Service1.Designer.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace WindowsServiceQ1_19
+{
+    partial class Service1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            this.ServiceName = "Service1";
+
+            string[] lines = { "1. First", "2. Second", "3. Third" };
+            File.AppendAllLines(sourceFile, lines);
+        }
+
+        #endregion
+    }
+}
---
+++ WindowsServiceQ1-19/WindowsServiceQ1-19/Service1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceQ1_19
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer t = null;
+        string filePath = "../../ServiceLog.txt";
+        string sourceFile = "../../Record.txt";
+
+        public Service1()
+        {
+            InitializeComponent();
+            t = new Timer();
+            t.Elapsed += new ElapsedEventHandler(Time_Elapsed);
+            t.Interval = 1 * 60 * 1000;
+            t.Enabled = true;
+            t.Start();
+        }
+
+        void Time_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            logData();
+        }
+
+        public void logData()
+        {
+            string[] arySchedule = File.ReadAllLines(sourceFile);
+            File.AppendAllLines(filePath, arySchedule);
+            File.Delete(sourceFile);
+        }
+        
+        protected override void OnStart(string[] args)
+        {
+            string[] lines = { "1. First", "2. Second", "3. Third" };
+            File.AppendAllLines(sourceFile, lines);
+        }
+
+        protected override void OnStop()
+        {
+            t.Stop();
+            t.Enabled = false;
+            File.AppendAllText(filePath, "Service stoped : " + DateTime.Now.ToString() + Environment.NewLine);
+        }
+    }
+}
